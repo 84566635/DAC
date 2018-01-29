020a414abfc17e9ce489abed2b6b15cfd2462f0f
Jacek Drewniak <jacek.drewniak@gmail.com>
2018-01-29 20:07:19 +0100

    MADO - initial compilation

--- library/inc/cfg.h
+++ library/inc/cfg.h
@@ -1,5 +1,11 @@
 #ifndef _CFG_H_
-#define _CFG_H_ 1
+#define _CFG_H_
+
+#define assert_param(cond) if(!(cond)){volatile int stop=1; while(stop);};
+#define configASSERT assert_param
+
+#define VERSION "MA_undefined"
+
 #define PRINT_CFG(arg_name) dprintf(LL_WARNING, "cfg.%s=0x%02x \n", #arg_name, cfg.arg_name)
 #define CFG_IDX(arg_cfg) (offsetof(config_t, arg_cfg)/(sizeof(int)))
 #define CFG_PATCH(arg_idx) (0xABCDEF00+(arg_idx))
