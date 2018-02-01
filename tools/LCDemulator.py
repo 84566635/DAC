0e2811bc4d5705aa5771623fdacf135a32d16dfe
OortJacek <jacek.drewniak@gmail.com>
2018-02-01 10:35:54 +0000

    Initial LCD emulator. Prints serial ports on host.

---
+++ tools/LCDemulator.py
@@ -0,0 +1,42 @@
+import sys
+import glob
+import serial.tools.list_ports
+
+
+def serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            
+            name = s.name;
+            s.close()
+            result.append(port)
+            result.append(name)
+        except (OSError, serial.SerialException):
+            pass
+        
+    for item in serial.tools.list_ports.comports():
+        print item
+    return result
+
+
+if __name__ == '__main__':
+    print(serial_ports())
